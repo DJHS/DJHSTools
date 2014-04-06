6577d590b61add0dafe740fde973b1749190a2b6
Tony Duco <nosduco@gmail.com>
2014-04-06 13:20:25 -0400

    Added LineInput and SystemOut.

---
+++ DJHSTools/src/djhstools/api/input/LineInput.java
@@ -0,0 +1,20 @@
+package djhstools.api.input;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class LineInput {
+	
+	private Scanner scan;
+	
+	public LineInput(InputStream stream){
+		this.scan = new Scanner(stream);
+	}
+	
+	public final String[] getInputLines(int amtLines){
+		String[] lines = new String[amtLines];
+		for(int i=0; i<amtLines; i++)
+			lines[i] = scan.nextLine();
+		return lines;
+	}
+}
---
+++ DJHSTools/src/djhstools/api/utilities/SystemOut.java
@@ -0,0 +1,14 @@
+package djhstools.api.utilities;
+
+public class SystemOut {
+	public SystemOut(){
+	}
+	
+	public final void p(String s){
+		System.out.print(s);
+	}
+	
+	public final void pl(String s){
+		System.out.println(s);
+	}
+}
